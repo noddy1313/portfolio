c0553090ae5afc256292a8d2b04c488b52d192af
Dev singh <mahgalcoco@gmail.com>
2025-10-08 15:22:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var Typed= new Typed(".text", {
+    strings:["AI Scripter", "AI Model Trainer", "AI Researcher"],
+    typeSpeed: 110,
+    backSpeed: 100,
+    backdDelay: 1000,
+    loop: true
+});

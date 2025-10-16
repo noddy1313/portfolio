159de06dc5ea3dd40b548ad853b8deb4b4eb545f
noddy1313 <mahgalcoco@gmail.com>
2025-10-17 00:31:43 +0530

    update

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var Typed= new Typed(".text", {
     strings:["AI Scripter", "AI Model Trainer", "AI Researcher"],
     typeSpeed: 110,
@@ -5,3 +6,16 @@ var Typed= new Typed(".text", {
     backdDelay: 1000,
     loop: true
 });
+=======
+var Typed= new Typed(".text", {
+    strings:["AI Scripter", "AI Model Trainer", "AI Researcher"],
+    typeSpeed: 110,
+    backSpeed: 100,
+    backdDelay: 1000,
+    loop: true
+});
+    AOS.init({
+        once: false,
+        mirror: true,
+    });
+>>>>>>> af34e61 (update)
